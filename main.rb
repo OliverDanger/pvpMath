a6ac7452a56d65861d81a627526a10491d3c506d
OliverDanger <od.oliverdixon@gmail.com>
2023-05-05 17:10:42 +0000

    add main.rb and move tests to main

---
+++ main.rb
@@ -0,0 +1,8 @@
+require './player'
+
+
+oliver = Person.new("OddOctopod")
+puts oliver.name
+puts oliver.lives
+oliver.lose_life
+puts oliver.lives
--- player.rb
+++ player.rb
@@ -12,9 +12,3 @@ class Person
   end
 
 end
-
-oliver = Person.new("OddOctopod")
-puts oliver.name
-puts oliver.lives
-oliver.lose_life
-puts oliver.lives
